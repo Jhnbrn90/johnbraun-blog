6d6a98fb9f1466dab102dc38ce594341745104bb
John Braun <jbraunnl@gmail.com>
2019-06-26 09:35:47 +0200

    Adjust mobile layout

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('content')
-<main class="container mx-auto px-48">
+<main class="container mx-auto lg:px-10 md:px-5 px-2">
     @include('blog.full-post', ['post' => $post])
 </main>
 @endsection
