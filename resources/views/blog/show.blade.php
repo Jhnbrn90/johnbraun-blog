c58a864a2f58818eddcf44ef5c26528c3504bb14
John Braun <jbraunnl@gmail.com>
2019-06-26 13:34:24 +0200

    Update styles

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('content')
-<main class="container mx-auto lg:px-20 md:px-10 px-4">
+<main class="container mx-auto lg:px-40 md:px-20 px-10">
     @include('blog.full-post', ['post' => $post])
 </main>
 @endsection
