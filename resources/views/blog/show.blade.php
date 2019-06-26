f8c8e38e2bd7c892b9d8a9987f5e18376507128a
John Braun <jbraunnl@gmail.com>
2019-06-26 09:40:35 +0200

    Fixed marigns

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('content')
-<main class="container mx-auto px-2">
+<main class="container mx-auto lg:px-10 md:px-5 px-2">
     @include('blog.full-post', ['post' => $post])
 </main>
 @endsection
