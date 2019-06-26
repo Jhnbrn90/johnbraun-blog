e3535593dd45904ef012d2d61e69bdf8bf108cae
John Braun <jbraunnl@gmail.com>
2019-06-26 09:53:15 +0200

    Still not satisfied with margins

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('content')
-<main class="container mx-auto lg:px-10 md:px-5 px-2">
+<main class="container mx-auto lg:px-20 md:px-10 px-4">
     @include('blog.full-post', ['post' => $post])
 </main>
 @endsection
