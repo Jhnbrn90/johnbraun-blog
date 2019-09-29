8cd1f813063403d02d4f9487b4dcc4c39ea80f06
John Braun <johnbraun@pm.me>
2019-09-29 15:33:17 +0200

    Add Twitter link to navbar

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
-    
+
     @yield('meta-tags')
 
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
@@ -32,9 +32,23 @@
     <header class="mb-10 pb-5 pt-8 shadow-lg bg-white px-10">
         <div class="flex container mx-auto lg:max-w-screen">
             <h1 class="flex w-full text-xl">
-                <a href="/" class="w-full text-center no-underline text-black font-semibold">
-                    John Braun<span class="text-sm font-thin">'s blog</span><img class="ml-5 inline h-12 w-12 shadow-lg rounded-full" src="{{ url('images/profile_resized.png') }}">
-                </a>
+
+                <div class="w-full text-center">
+                    <img class="inline sm:hidden mr-1 h-8 w-8 shadow-lg rounded-full" src="{{ url('images/profile_resized.png') }}">
+                    <a href="/" class="no-underline text-black font-semibold">
+                        John Braun<span class="text-sm font-thin">'s blog</span>
+                        <img class="sm:inline hidden ml-5 h-12 w-12 shadow-lg rounded-full" src="{{ url('images/profile_resized.png') }}">
+                    </a>
+
+                    <div class="sm:block hidden flex justify-center flex-wrap text-xs tracking-tight font-semibold ml-1 align-top inline">
+                        <a class="flex-1/2" href="https://twitter.com/jhnbrn90" rel="me">
+                            <svg class="inline-block w-6 h-6" viewBox="0 0 400 400">
+                                <path style="fill: #1da1f2;" d="M153.62,301.59c94.34,0,145.94-78.16,145.94-145.94,0-2.22,0-4.43-.15-6.63A104.36,104.36,0,0,0,325,122.47a102.38,102.38,0,0,1-29.46,8.07,51.47,51.47,0,0,0,22.55-28.37,102.79,102.79,0,0,1-32.57,12.45,51.34,51.34,0,0,0-87.41,46.78A145.62,145.62,0,0,1,92.4,107.81a51.33,51.33,0,0,0,15.88,68.47A50.91,50.91,0,0,1,85,169.86c0,.21,0,.43,0,.65a51.31,51.31,0,0,0,41.15,50.28,51.21,51.21,0,0,1-23.16.88,51.35,51.35,0,0,0,47.92,35.62,102.92,102.92,0,0,1-63.7,22A104.41,104.41,0,0,1,75,278.55a145.21,145.21,0,0,0,78.62,23"/>
+                            </svg>
+                            <span class="align-center -ml-1">twitter</span>
+                        </a>
+                    </div>
+                </div>
 
                 <div class="flex flex-1 justify-end">
                     <a href="/about" class="no-underline text-black font-semibold">
