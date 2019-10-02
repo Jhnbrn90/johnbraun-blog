4237fe6f18fdfd260853d726823a67cc1afad70b
John Braun <johnbraun@pm.me>
2019-10-02 10:34:58 +0200

    Add Cache headers

---
+++ app/Http/Middleware/SetCacheHeaders.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class SetCacheHeaders
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        $response->header('Cache-Control', 'max-age=604800');
+
+        return $response;
+    }
+}
