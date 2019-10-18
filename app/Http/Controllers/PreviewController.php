c0960736116ce7ddedba13e15f93167a84f7e478
John Braun <johnbraun@pm.me>
2019-10-18 13:06:17 +0200

    Update preview

--- app/Http/Controllers/PreviewController.php
+++ app/Http/Controllers/PreviewController.php
@@ -10,11 +10,11 @@ class PreviewController extends Controller
     public function show($slug)
     {
         $post = Post::draft()->whereSlug($slug)->first();
-        
+
         if (! $post) {
             abort(404);
         }
 
-        return view('preview.show', compact('post'));
+        return view('blog.show', compact('post'));
     }
 }
